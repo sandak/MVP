67e06aa0143a119b68419bdc36c2276422c81880
amit sandak <sandak.amit@gmail.com>
2015-09-19 00:42:03 +0300

    possiblemoves fix

--- MVP/src/GuiView/PossibleMoves.java
+++ MVP/src/GuiView/PossibleMoves.java
@@ -31,16 +31,34 @@ public class PossibleMoves extends Canvas {
 			{
 			    Image image = new Image(getDisplay(),"resources/rightGreen.png");
 				  // 	e.gc.drawImage(image, 0, 0);
-				    Image scaledGC = new Image(getDisplay(),(int)(canvas.getSize().x*0.1),(int)(canvas.getSize().y*0.1));
+				    Image scaledGC = new Image(getDisplay(),(int)(canvas.getSize().x*0.5),(int)(canvas.getSize().y*0.5));
 				    //GC gc = new GC(scaledGC);
-				    e.gc.drawImage(image,0,0,canvas.getSize().x,canvas.getSize().y,0,0,(int)(canvas.getSize().x*0.2),(int)(canvas.getSize().x*0.2));
+				    e.gc.drawImage(image,0,0,canvas.getSize().x,canvas.getSize().y,0,0,(int)(canvas.getSize().x*0.5),(int)(canvas.getSize().x*0.5));
 			}
 			else
 			{
 				  Image image = new Image(getDisplay(),"resources/rightRed.png");
-				   //	e.gc.drawImage(image, 0, 0);
-				    Image scaledGC = new Image(getDisplay(),(int)(canvas.getSize().x*0.1),(int)(canvas.getSize().y*0.1));
-				   e.gc.drawImage(image,0,0,canvas.getSize().x,canvas.getSize().y,0,0,(int)(canvas.getSize().x*1),(int)(canvas.getSize().x*1));
+	
+					    final int width = image.getBounds().width;
+					    final int height = image.getBounds().height;
+
+					final Image scaled050 = new Image(getDisplay(),
+					        image.getImageData().scaledTo((int)(width*0.5),(int)(height*0.5)));
+					    final Image scaled200 = new Image(getDisplay(),
+					        image.getImageData().scaledTo((int)(width*2),(int)(height*2)));
+
+					final Image scaledGC200 = new Image(getDisplay(),(int)(width*2),(int)(height*2));
+					    GC gc = new GC(scaledGC200);
+					    gc.drawImage(image,0,0,width,height,0,0,width*2,height*2);
+					    
+					     e.gc.drawImage(image,0,0,width,height,0,0,(int)(width*0.5),(int)(height*0.5));
+					        e.gc.drawImage(scaled050,100,0);
+					        e.gc.drawImage(scaledGC200,0,75);
+					        e.gc.drawImage(scaled200,225,175);
+				  
+				  // Image scaledGC = new Image(getDisplay(),(int)(canvas.getSize().x*0.3),(int)(canvas.getSize().y*0.3));
+				 // e.gc.drawImage(image,0,0,canvas.getSize().x,canvas.getSize().y,0,0,(int)(canvas.getSize().x*0.3),(int)(canvas.getSize().x*0.3));
+		
 			}
 
 				
