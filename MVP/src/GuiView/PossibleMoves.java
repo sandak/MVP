4f83b4c48af8274e1a01ba26bf827dfefb52a568
amit sandak <sandak.amit@gmail.com>
2015-09-19 02:26:03 +0300

    possiblemoves widget fix

--- MVP/src/GuiView/PossibleMoves.java
+++ MVP/src/GuiView/PossibleMoves.java
@@ -5,6 +5,7 @@ import org.eclipse.swt.events.PaintEvent;
 import org.eclipse.swt.events.PaintListener;
 import org.eclipse.swt.graphics.Color;
 import org.eclipse.swt.graphics.Image;
+import org.eclipse.swt.layout.GridData;
 import org.eclipse.swt.layout.GridLayout;
 import org.eclipse.swt.widgets.Canvas;
 import org.eclipse.swt.widgets.Composite;
@@ -15,36 +16,41 @@ public class PossibleMoves extends Canvas {
 		super(parent, style);
 		setBackground(new Color(null, 255, 255, 255));
 		Canvas canvas = this;
-		canvas.setLayout(new GridLayout(4,false));
-		new Arrow(canvas,"resources/upGreen.png","resources/rightRed.png", SWT.FILL);
-		new RightArrow(canvas, SWT.FILL);
-    	addPaintListener(new PaintListener() {
-			
-			@Override
-			public void paintControl(PaintEvent e) {
-				
-				//GC gc = new GC(canvas); 
-				
-				Image image;
-			if(canvas.getParent().getSize().x>400)
-				   image = new Image(getDisplay(),"resources/rightGreen.png");
-			else
-				   image = new Image(getDisplay(),"resources/rightRed.png");
-
-		     int imageWidth = image.getBounds().width;
-		     int imageHeight = image.getBounds().height;
-		     int width = canvas.getSize().x;
-		     int height = canvas.getSize().y;
-
-		 //Image scaled050 = new Image(getDisplay(),image.getImageData().scaledTo(width,height));
-		// Image scaledGC200 = new Image(getDisplay(),width,height);    
-		     e.gc.drawImage(image,0,0,imageWidth,imageHeight,0,0,width,height);
-				
-				  
-					}
-				      
-				
-		});
+		canvas.setLayout(new GridLayout(3,false));
+		new Arrow(canvas,"resources/upGreen.png","resources/upRed.png", SWT.FILL).setLayoutData(new GridData(SWT.FILL, SWT.FILL, true, true, 1, 1));
+		new Arrow(canvas,"resources/upGreen.png","resources/upRed.png", SWT.FILL).setLayoutData(new GridData(SWT.CENTER, SWT.FILL, true, true, 2, 1));
+		new Arrow(canvas,"resources/downGreen.png","resources/downRed.png", SWT.FILL).setLayoutData(new GridData(SWT.FILL, SWT.TOP, true, true, 1, 2));
+		new Arrow(canvas,"resources/leftGreen.png","resources/leftRed.png", SWT.FILL).setLayoutData(new GridData(SWT.FILL, SWT.FILL, true, true, 1, 1));
+		new Arrow(canvas,"resources/rightGreen.png","resources/rightRed.png", SWT.FILL).setLayoutData(new GridData(SWT.FILL, SWT.FILL, true, true, 1, 1));
+		new Arrow(canvas,"resources/downGreen.png","resources/downRed.png", SWT.FILL).setLayoutData(new GridData(SWT.CENTER, SWT.CENTER, true, true, 2, 1));
+		//new RightArrow(canvas, SWT.FILL).setLayoutData(new GridData(SWT.FILL, SWT.FILL, true, true, 1, 1));
+ //   	addPaintListener(new PaintListener() {
+//			
+//			@Override
+//			public void paintControl(PaintEvent e) {
+//				
+//				//GC gc = new GC(canvas); 
+//				
+//				Image image;
+//			if(canvas.getParent().getSize().x>400)
+//				   image = new Image(getDisplay(),"resources/rightGreen.png");
+//			else
+//				   image = new Image(getDisplay(),"resources/rightRed.png");
+//
+//		     int imageWidth = image.getBounds().width;
+//		     int imageHeight = image.getBounds().height;
+//		     int width = canvas.getSize().x;
+//		     int height = canvas.getSize().y;
+//
+//		 //Image scaled050 = new Image(getDisplay(),image.getImageData().scaledTo(width,height));
+//		// Image scaledGC200 = new Image(getDisplay(),width,height);    
+//		     e.gc.drawImage(image,0,0,imageWidth,imageHeight,0,0,width,height);
+//				
+//				  
+//					}
+//				      
+//				
+//		});
     	
  }
 
